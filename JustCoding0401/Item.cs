7209ff6278495b683f1044bbcba60b30fd363162
Jonathan Lindholm <109154307+JonOganesson@users.noreply.github.com>
2023-04-17 16:09:23 +0200

    Add files via upload

---
+++ JustCoding0401/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustCoding0401
+{
+    internal class Item
+    {
+        private string EmployeeName { get; set; }
+        private string ProductName { get; set; }
+        private string Description { get; set; }
+        private double Price { get; set; }
+        private int Quantity { get; set; }
+
+        public Item(string pName, string description, double price, int quantity)
+        {
+            ProductName = pName;
+            this.Description = description;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"Product Name: {ProductName}\nDescription: {Description}\nPrice: {Price:C}\nQuantity: {Quantity}\n";
+        }
+    }
+}
---
+++ JustCoding0401/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace JustCoding0401
+{
+    //A lucid inventory management system
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] items = new Item[3];
+
+            Console.Write("Employee Name: ");
+            string eName = Console.ReadLine();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"\nEnter details for item {i + 1}:");
+                Console.Write("Product Name: ");
+                string pName = Console.ReadLine();
+                Console.Write("Description: ");
+                string description = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                items[i] = new Item(pName, description, price, quantity);
+            }
+
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            Console.WriteLine("Inventory Received: \n");
+            Console.WriteLine($"Inventory received by: {eName}");
+
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
