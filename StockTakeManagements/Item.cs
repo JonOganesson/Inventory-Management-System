14253efd228e2a11629b1dbe3f0f59ded02df0a7
Jonathan Lindholm <109154307+JonOganesson@users.noreply.github.com>
2023-04-28 09:48:01 +0200

    Add files via upload

---
+++ StockTakeManagements/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTakeManagements
+{
+    internal class Item
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+
+        public Item(string pName, string description, double price, double quantity)
+        {
+            ProductName = pName;
+            this.Description = description;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+        public override string ToString()
+        {
+            string.Format("R0.00", Price);
+            string.Format("00.00g", Quantity);
+            return ($"Product Name: {ProductName}\nDescription: {Description}\nPrice: {Price}\nQuantity: {Quantity}\n");
+        }
+    }
+
+}
---
+++ StockTakeManagements/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+
+namespace StockTakeManagements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            DateTime dateTime = DateTime.Now;
+            Item[] items = new Item[1000000];
+
+            Console.Write("Employee Name: ");
+            string eName = Console.ReadLine();
+
+            WhileLoop:
+            while (true)
+            {
+                Console.WriteLine("Press 'Enter' to start, and type 'Exit' and press 'Enter' to stop.");
+                userInput = Console.ReadLine();
+            
+                if (string.IsNullOrEmpty(userInput))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Leave the 'Product Name' empty and press 'Enter' to stop.");
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        Console.WriteLine($"\nEnter details for item {i + 1}:");
+                        Console.Write("Product Name: ");
+                        string pName = Console.ReadLine();
+                        if (string.IsNullOrEmpty(pName))
+                        {
+                            Console.Clear();
+                            goto WhileLoop;
+                        }
+                        Console.Write("Description: ");
+                        string description = Console.ReadLine();
+                        Console.Write("Price: ");
+                        double price = double.Parse(Console.ReadLine());
+                        price = Math.Round(price, 2);
+                        Console.Write("Quantity: ");
+                        double quantity = double.Parse(Console.ReadLine());
+                        quantity = Math.Round(quantity, 2);
+
+                        items[i] = new Item(pName, description, price, quantity);
+                    }
+                }
+                
+                else if (userInput.ToLower() == "exit")
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            Console.WriteLine("Inventory Recorded: \n");
+            Console.WriteLine($"Inventory Recorded by: {eName}");
+            Console.WriteLine($"Inventory Recorded at: {dateTime}\n");
+
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
